491134ba21477a5a2c50e48ebbf9b4abd75abb94
Elizabeth-Garibaldi <elizabeth.garibaldi@payclip.com>
2023-09-05 12:23:49 -0500

    add lightning-datatable

--- force-app/main/default/lwc/transactions/transactions.js
+++ force-app/main/default/lwc/transactions/transactions.js
@@ -1,81 +1,59 @@
 import { LightningElement, track, wire } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
+import retriveTransactions from '@salesforce/apex/TransactionsController.retriveTransactions';
+import {loadStyle} from 'lightning/platformResourceLoader';
+import CSS from '@salesforce/resourceUrl/txnCss';
+import retriveTxn from '@salesforce/apex/TransactionsController.retriveTxn';
 
-import retriveTransactions from '@salesforce/apex/TransactionsTableController.retriveTransactions';
-// import retriveTxn from '@salesforce/apex/TransactionsTableController.retriveTxn';
-export default class Transactions extends LightningElement {
+const columns =[
+  {label: 'Txn Date' , fieldName: 'date_time', type:'date', cellAttributes:{
+    class:{fieldName:'dateColor'},
+    alignment: 'center',
+  }},
+  {label: 'Payment Date' , fieldName: 'formattedDate', type:'text', cellAttributes: { alignment: 'center'}}, 
+  {label: 'Card' , fieldName: 'last4' , cellAttributes:{
+    iconName:{fieldName:'iconName'}, 
+    iconPosition:'left',
+    alignment: 'center',
+  }},
+  {label: 'Charged with' , fieldName: 'reader_type_code', cellAttributes: { alignment: 'center'}},
+  {label: 'Issuer' , fieldName: 'issuer', cellAttributes: { alignment: 'center'}},
+  {label: 'Receipt Num' , fieldName: 'receipt_no', cellAttributes: { alignment: 'center'}},
+  {label: 'Cashier' , fieldName: 'cashier', type: 'email', cellAttributes: { alignment: 'center'}},
+  {label: 'Status' , fieldName: 'status_message', cellAttributes:{
+    class:{
+      fieldName:'statusColor',
+      label: 'slds-text-align_center',
+    },
+    alignment: 'center',
+  }},
+  {label: 'Txn ID' , fieldName: 'transaction_id', cellAttributes: { alignment: 'center'}},
+  {label: 'Term' , fieldName: 'term', type: 'text', cellAttributes: { alignment: 'center'}},
+  {label: 'Subtotal' , fieldName: 'base_amount', type:'currency', cellAttributes: { 
+    alignment: 'center',
+    class: 'slds-text-align_center',
+  }},
+  {label: 'Total' , fieldName: 'amount', type: 'currency', cellAttributes: { alignment: 'center'}},
+]
+export default class TxnTable extends LightningElement {
   @track error;
-  merchantId;
-  @track transactions = []
-  headings=["Txn Date","Payment Date", "Card", "Charged with", "Issuer", "Receipt Num", "Cashier", "Status", "Txn ID", "Term", "Subtotal", "Total"];
+  @track transactions
+  @wire(CurrentPageReference)
+  currentPageReference;
+  receiptNum;
   filterBy="receipt_no"
+  @track searchValue = '';
   showSpinner = false;
   timer;
-  @wire(CurrentPageReference)
-    currentPageReference;
+  columns = columns;
+  isCssLoaded = false;
   connectedCallback() {
     this.showSpinner = true;
-    if ( this.currentPageReference.state.c__merchantId ) {
-      this.merchantId = this.currentPageReference.state.c__merchantId;
-      this.fetchData(this.merchantId);
+    if ( this.currentPageReference.state.c__receiptNum) {
+      this.receiptNum = this.currentPageReference.state.c__receiptNum;
+      this.handleSearch(this.receiptNum);
     }
-    // this.fetchFilteredData(retriveTxn);
-    // this.fetchFilteredData(retriveTransactions, true);
   }
-
-  fetchData(merchantId){
-    console.log(merchantId);
-    retriveTransactions({merchantId: merchantId})
-    .then(response => {
-      this.transactions = response.transactions;
-      console.log(response);
-    })
-    .catch(error => {
-      this.handleError(error)
-      console.log(error)})
-    .finally(() => {
-      this.showSpinner = false;
-    });
-  }
-  fetchFilteredData(apexMethod, isSecondData = false) {
-    apexMethod()
-      .then(response => {
-        this.formatTxnsData(response.transactions);
-        if (isSecondData) {
-          this.fullTableData2 = response.transactions;
-        } else {
-          this.fullTableData = response.transactions;
-        }
-        console.log(response);
-      })
-      .catch(error => this.handleError(error))
-      .finally(() => {
-        this.showSpinner = false;
-      });
-  }
-
-formatTxnsData(res) {
-  this.transactions = res.map((item, index) => {
-    // Formatear fecha
-    const formattedDateTime = this.formatDateTime(item.date_time);
-    const date = new Date(item.date * 1000).toDateString();
-    const amount = item.amount;
-    return { ...item, id: `new_${index + 1}`, amount, date, date_time: formattedDateTime };
-  });
-}
-
-formatDateTime(dateTimeString) {
-  const date_time = new Date(dateTimeString);
-  const options = {
-    day: 'numeric',
-    month: 'short',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: true,
-  };
-  return new Intl.DateTimeFormat('en-US', options).format(date_time);
-}
 
 handleError(error) {
   this.error = error;
@@ -90,20 +68,83 @@ handleError(error) {
     ]
   }
 
+  get isButtonDisabled() {
+    return this.searchValue.trim() === '';
+}
+
   filterbyHandler(event){
     this.filterBy = event.target.value
   }
 
-  filterHandler(event){
-    const {value} = event.target
-    window.clearTimeout(this.timer)
-    if(value){
-      // eslint-disable-next-line @lwc/lwc/no-async-operation
-      this.timer = window.setTimeout(()=>{
-        this.transactions = this.fullTableData2
-      }, 500)
-    } else {
-      this.transactions = [...this.fullTableData]
-    }
-  }
+  handleSearchChange(event) {
+    this.searchValue = event.target.value;
+    console.log(this.searchValue, 'searchvalue');
+}
+
+fetchData() {
+  retriveTxn()
+      .then(data => {
+        this.transactions = data.transactions.map(item=>{
+          let statusColor = item.status_message === "Error"?"slds-text-color_error":"slds-text-color_success"
+          let iconName = item.last4 <500000 ? "custom:custom40":"utility:up"
+          const formattedDate = this.formatDate(item.date);
+          return {...item, 
+              "statusColor":statusColor,
+              "iconName":iconName,
+              "dateColor": "datatable-color",
+              formattedDate: formattedDate,
+              term: item.term ? item.term + 'MSI' : 'REG',
+          }
+      })
+      console.log(this.transactions)
+      })
+      .catch(error => {
+        this.handleError(error)
+      })
+      .finally(() => {
+        this.showSpinner = false;
+      });
+}
+
+handleSearch() {
+  retriveTransactions({ receiptNum: this.searchValue })
+      .then(data => {
+        this.transactions = data.transactions.map(item=>{
+          let statusColor = item.status_message === "Error"?"slds-text-color_error":"slds-text-color_success"
+          let iconName = item.last4 <500000 ? "custom:custom40":"utility:up"
+          const formattedDate = this.formatDate(item.date);
+          return {...item, 
+              "statusColor":statusColor,
+              "iconName":iconName,
+              "dateColor": "datatable-color",
+              formattedDate: formattedDate,
+              term: item.term ? item.term + 'MSI' : 'REG',
+          }
+      })
+      console.log(this.transactions)
+      })
+      .catch(error => {
+        this.handleError(error)
+      })
+      .finally(() => {
+        this.showSpinner = false;
+      });
+}
+
+renderedCallback(){
+  if(this.isCssLoaded) return
+  this.isCssLoaded = true
+  loadStyle(this, CSS).then(()=>{
+    console.log('Loaded successfully');
+  }).catch(error => {
+    this.handleError(error)
+  })
+}
+
+formatDate(timestamp) {
+  const date = new Date(timestamp * 1000); // Convertir el timestamp Unix a milisegundos
+  const options = { year: 'numeric', month: 'short', day: 'numeric' };
+  return date.toLocaleDateString('es-ES', options); // Cambiar 'es-ES' al código del idioma deseado
+}
+
 }
